64f6b2867f723e08b5d617e58d60a9ce779d4e6e
Marko Petrlić <petrlic.marko@gmail.com>
2024-08-07 11:52:03 +0200

    Bumped jsonrpsee to v0.22 and added rpc limiter from upstream (#14)

--- substrate/client/cli/src/config.rs
+++ substrate/client/cli/src/config.rs
@@ -33,7 +33,7 @@ use sc_service::{
 	BlocksPruning, ChainSpec, TracingReceiver,
 };
 use sc_tracing::logging::LoggerBuilder;
-use std::{net::SocketAddr, path::PathBuf};
+use std::{net::SocketAddr, num::NonZeroU32, path::PathBuf};
 
 /// The maximum number of characters for a node name.
 pub(crate) const NODE_NAME_MAX_LENGTH: usize = 64;
@@ -338,6 +338,11 @@ pub trait CliConfiguration<DCV: DefaultConfigurationValues = ()>: Sized {
 		Ok(RPC_DEFAULT_MESSAGE_CAPACITY_PER_CONN)
 	}
 
+	/// Rate limit calls per minute.
+	fn rpc_rate_limit(&self) -> Result<Option<NonZeroU32>> {
+		Ok(None)
+	}
+
 	/// Get the prometheus configuration (`None` if disabled)
 	///
 	/// By default this is `None`.
@@ -510,6 +515,7 @@ pub trait CliConfiguration<DCV: DefaultConfigurationValues = ()>: Sized {
 			rpc_max_subs_per_conn: self.rpc_max_subscriptions_per_connection()?,
 			rpc_port: DCV::rpc_listen_port(),
 			rpc_message_buffer_capacity: self.rpc_buffer_capacity_per_connection()?,
+			rpc_rate_limit: self.rpc_rate_limit()?,
 			prometheus_config: self
 				.prometheus_config(DCV::prometheus_listen_port(), &chain_spec)?,
 			telemetry_endpoints,
@@ -615,14 +621,15 @@ pub trait CliConfiguration<DCV: DefaultConfigurationValues = ()>: Sized {
 		logger.init()?;
 
 		match fdlimit::raise_fd_limit() {
-			Ok(fdlimit::Outcome::LimitRaised { to, .. }) =>
+			Ok(fdlimit::Outcome::LimitRaised { to, .. }) => {
 				if to < RECOMMENDED_OPEN_FILE_DESCRIPTOR_LIMIT {
 					warn!(
 						"Low open file descriptor limit configured for the process. \
 						Current value: {:?}, recommended value: {:?}.",
 						to, RECOMMENDED_OPEN_FILE_DESCRIPTOR_LIMIT,
 					);
-				},
+				}
+			},
 			Ok(fdlimit::Outcome::Unsupported) => {
 				// Unsupported platform (non-Linux)
 			},
@@ -648,7 +655,7 @@ pub fn generate_node_name() -> String {
 		let count = node_name.chars().count();
 
 		if count < NODE_NAME_MAX_LENGTH {
-			return node_name
+			return node_name;
 		}
 	}
 }
